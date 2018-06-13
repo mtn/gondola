8be226634f32ca2e3c0d6e0120c6c1a504a2ec85
Michael Noronha <michaeltnoronha@gmail.com>
2018-06-13 00:21:08 -0500

    Group request and log types together

--- src/log.py
+++ src/log.py
@@ -1,7 +1,19 @@
 "A node log entry"
 
 # pylint: disable=too-few-public-methods
+# pylint: disable=missing-docstring
 
+
+class Request(object):
+    pass
+
+
+class SetRequest(Request):
+    pass
+
+
+class GetRequest(Request):
+    pass
 
 class LogEntry(object):
     "An entry in a node's log"
